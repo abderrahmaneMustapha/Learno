158978c394828688fa43acb2edf9af96327e7681
abderrahmane <abderrahmanemustapha030898@gmail.com>
2019-03-22 21:51:10 +0100

    remigrate

--- learno/settings.py
+++ learno/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'background_task',
     'social_django',  # <--
 
     'accounts',
@@ -50,7 +51,7 @@ INSTALLED_APPS = [
     'colorful',
 
     'rest_framework',
-    'background_task',
+
 
 ]
 
