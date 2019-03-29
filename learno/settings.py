0da708de373b7e5b2f01743030b4f2cf90863d87
abderrahmane <abderrahmanemustapha030898@gmail.com>
2019-03-29 16:32:02 +0100

    first try to add background task to heroku

--- learno/settings.py
+++ learno/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+     'background_task',
     'accounts',
     'ide',
     'course',
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'django_heroku',
     'rest_framework',
     'social_django',  # <--
+
 
 
 
