8e98f9d7ae2875d4c695b698bcf09178b2d10e01
abderrahmane <abderrahmanemustapha030898@gmail.com>
2019-03-29 02:03:28 +0100

    fix heroku 6

--- accounts/migrations/0001_initial.py
+++ accounts/migrations/0001_initial.py
@@ -11,7 +11,8 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+
     ]
 
     operations = [
