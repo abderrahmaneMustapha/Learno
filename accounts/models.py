d9bb320eeec2bd4e1b51ac1559ed74f65d2583e2
abderrahmane <abderrahmanemustapha030898@gmail.com>
2019-03-30 23:29:01 +0100

    fix create a student secend test 0

--- accounts/models.py
+++ accounts/models.py
@@ -144,9 +144,6 @@ class Student(models.Model):
     def create_user_profile(sender, instance, created, **kwargs):
         if created:
             Profile.objects.create(user=instance)
-    @receiver(post_save, sender=User)
-    def save_user_profile(sender, instance, **kwargs):
-        instance.profile.save()
 
 def calculate_rank():
 
