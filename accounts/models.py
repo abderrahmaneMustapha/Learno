dd93f4ea72d8af488ef61486dfedd1df67beb416
abderrahmane <abderrahmanemustapha030898@gmail.com>
2019-03-30 23:21:28 +0100

    fix create a student secend test

--- accounts/models.py
+++ accounts/models.py
@@ -142,8 +142,8 @@ class Student(models.Model):
         return level
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
+        if created:
+            Profile.objects.create(user=instance)
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
