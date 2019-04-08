ab8ba907bbe31473b0b1d552dc10dda0a77e17b4
abderrahmane <abderrahmanemustapha030898@gmail.com>
2019-04-08 22:54:07 +0100

    add contact us template

--- learno/settings.py
+++ learno/settings.py
@@ -206,7 +206,8 @@ SERVE_MEDIA_FILES = True
 
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'abderrahmanemustapha030898@gmail.com'
 EMAIL_HOST_PASSWORD = 'Abdou030898mA'
