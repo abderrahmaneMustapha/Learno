259f8277b84e2252f67ce7e8d149d07bf90b37c6
abderrahmane <abderrahmanemustapha030898@gmail.com>
2019-04-11 01:08:09 +0100

    user can share update edit codes

--- ide/migrations/0005_auto_20190411_0004.py
+++ ide/migrations/0005_auto_20190411_0004.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('ide', '0004_auto_20190410_2209'),
+        ('ide', '0003_auto_20190410_2159'),
     ]
 
     operations = [
