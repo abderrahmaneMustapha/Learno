ef25ab2ae4dab683b9d851dec42f86b441ed26a5
abderrahmane <abderrahmanemustapha030898@gmail.com>
2019-04-15 00:17:40 +0100

    redis fixing

--- learno/settings.py
+++ learno/settings.py
@@ -251,17 +251,13 @@ GOOGLE_RECAPTCHA_SECRET_KEY = '6LfBWpYUAAAAAJTyRkqWJ6IKktj7Cbe5upwZfasi'
 """
 celery configuration
 """
-redis_host = os.environ.get('REDIS_HOST', 'localhost')
-# Channel layer definitions
-# http://channels.readthedocs.org/en/latest/deploying.html#setting-up-a-channel-backend
 CHANNEL_LAYERS = {
     "default": {
-        # This example app uses the Redis channel layer implementation asgi_redis
         "BACKEND": "asgi_redis.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [(redis_host, 6379)],
+            "hosts": [os.environ.get('REDISCLOUD_URL', 'redis://localhost:6379')],
         },
-        "ROUTING": "multichat.routing.channel_routing",
+        "ROUTING": "chat.routing.channel_routing",
     },
 }
 from celery.schedules import crontab
